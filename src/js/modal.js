7d42bb983879277d1446be1ca511d97c3d94c6aa
kristinka_skl <kristinkaskl@gmail.com>
2025-05-18 19:56:37 +0200

    add modal js

---
+++ src/js/modal.js
@@ -0,0 +1,47 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-s]"),
+      closeModalBtn: document.querySelector("[data-modal-close-s]"),
+      modal: document.querySelector("[data-modal-s]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-open");
+      document.body.classList.toggle("no-scroll");
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-m]"),
+      closeModalBtn: document.querySelector("[data-modal-close-m]"),
+      modal: document.querySelector("[data-modal-m]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-open");
+      document.body.classList.toggle("no-scroll");
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-l]"),
+      closeModalBtn: document.querySelector("[data-modal-close-l]"),
+      modal: document.querySelector("[data-modal-l]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-open");
+      document.body.classList.toggle("no-scroll");
+    }
+  })();
