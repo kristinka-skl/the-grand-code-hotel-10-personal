8c0a6087d21371c863eeacc1cd2ea12822765149
kristinka-skl <kristinka.skl@gmail.com>
2025-05-19 09:31:19 +0200

    fix open modal btns in js

--- src/js/modal.js
+++ src/js/modal.js
@@ -32,12 +32,12 @@
 
   (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open-l]"),
+      openModalBtns: document.querySelectorAll("[data-modal-open-l]"),
       closeModalBtn: document.querySelector("[data-modal-close-l]"),
       modal: document.querySelector("[data-modal-l]"),
     };
   
-    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtns.forEach((openModalBtn) => openModalBtn.addEventListener("click", toggleModal));
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
